23a2f96147eb4316ea6ceac3038f4b9b9aa168ec
manzoDev <manzoroger08@gmail.com>
2024-04-30 12:47:44 -0400

    feat code

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Task 📄
-## Aplicación de lista de tareas utilizando React y TypeScript
+## Aplicación de lista de tareas utilizando React (Node V.16) y TypeScript.
 
 ### La aplicación tendrá un pequeño formulario que permite a los usuarios agregar nuevas tareas. Cada tarea tendrá un casillero para marcarla como completada y un botón para eliminarla.
 ### Utilizar Bootswatch para estilizar la aplicación
+
+![alt text](image.png)
+
+
---
+++ src/App.tsx
@@ -0,0 +1,85 @@
+import React, { useState, useRef } from "react";
+
+type FormElement = React.FormEvent<HTMLFormElement>;
+
+interface ITask {
+  name: string;
+  done: boolean;
+}
+
+function App(): JSX.Element {
+  const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const taskInput = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: FormElement) => {
+    e.preventDefault();
+    addTask(newTask);
+    setNewTask("");
+    taskInput.current?.focus();
+  };
+
+  const addTask = (name: string): void => {
+    const newTask = [...tasks, { name, done: false }];
+    setTasks(newTask);
+  };
+
+  const toggleDoneTask = (i: number): void => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks[i].done = !newTasks[i].done;
+    setTasks(newTasks);
+  };
+
+  const removeTask = (i: number) => {
+    const newTasks: ITask[] = [...tasks]
+    newTasks.splice(i,1);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  onChange={(e) => setNewTask(e.target.value)}
+                  value={newTask}
+                  className="form-control"
+                  ref={taskInput}
+                  autoFocus
+                />
+                <button className="btn btn-success btn-block mt-2">send</button>
+              </form>
+            </div>
+          </div>
+          {tasks.map((t: ITask, i: number) => (
+            <div className="card card-body mt-2" key={i}>
+              <h2 style={{ textDecoration: t.done ? "line-through" : "" }}>
+                {t.name}
+              </h2>
+              <div>
+                <button
+                  className="btn btn-secondary"
+                  onClick={() => toggleDoneTask(i)}
+                >
+                  {t.done ? "✓" : "𝘟"}
+                </button>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => removeTask(i)}
+                >
+                  🗑️
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
